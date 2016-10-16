fa03efc44d6456cb21c8d2fd91c1ec0d9798ce8b
ahmdrz <ahmadrezazibaei@hotmail.com>
2016-10-16 12:28:29 +0330

    example fixed

--- example/tagliker/main.go
+++ example/tagliker/main.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/ahmdrz/goinsta/src"
@@ -17,42 +16,17 @@ func main() {
 
 	defer insta.Logout()
 
-	bytes, err := insta.TagFeed("pizza")
+	resp, err := insta.TagFeed("pizza")
 	if err != nil {
 		panic(err)
 	}
 
-	type Caption struct {
-		Status string `json:"status"`
-		Text   string `json:"text"`
-	}
-
-	type Item struct {
-		Id        string  `json:"id"`
-		Caption   Caption `json:"caption"`
-		LikeCount int     `json:"like_count"`
-	}
-
-	var Result struct {
-		Status string `json:"status"`
-		Items  []Item `json:"ranked_items"`
-	}
-
-	err = json.Unmarshal(bytes, &Result)
-	if err != nil {
-		panic(err)
-	}
-
-	if Result.Status != "ok" {
-		panic("Error occured , " + Result.Status)
-	}
-
-	for _, item := range Result.Items {
-		_, err := insta.Like(item.Id)
+	for _, item := range resp.Items {
+		_, err := insta.Like(item.ID)
 		if err != nil {
-			fmt.Println("ID : ", item.Id, "can't like")
+			fmt.Println("ID : ", item.ID, "can't like")
 		} else {
-			fmt.Println("ID : ", item.Id, "liked")
+			fmt.Println("ID : ", item.ID, "liked")
 		}
 	}
 }
