a409f99c29cebc97d5602bd5db208dcf6ecf7622
Ahmadreza Zibaei <ahmdrz@users.noreply.github.com>
2017-04-02 15:20:08 +0430

    Update shortid_test.go

--- shortid_test.go
+++ shortid_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 )
 
-func TestFromCode(t *testing.T) {
-	mediaID := FromCode("BR_repxhx4O")
+func TestMediaFromCode(t *testing.T) {
+	mediaID := MediaFromCode("BR_repxhx4O")
 	if mediaID == "1477090425239445006" {
 		t.Log(mediaID)
 	} else {
