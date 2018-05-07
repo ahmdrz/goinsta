8b2646b16d9a60cb0ecd3c81307584aa16ebfd90
themester <dgrripoll@gmail.com>
2018-05-07 14:54:09 +0000

    Defined account object

--- account.go
+++ account.go
@@ -3,6 +3,23 @@ package goinsta
 // Account is personal account object
 type Account struct {
 	inst *Instagram
+
+	// Activity is recent activity
+	Activity *Activity
+	// Tray is your disponible friend's stories
+	Tray *Tray
+
+	User
+	//Birthday -> what the hell is ?
+	PhoneNumber             string           `json:"phone_number"`
+	HDProfilePicVersions    []ImageCandidate `json:"hd_profile_pic_versions"`
+	Gender                  int              `json:"gender"`
+	ShowConversionEditEntry bool             `json:"show_conversion_edit_entry"`
+	ExternalLynxURL         string           `json:"external_lynx_url"`
+	Biography               string           `json:"biography"`
+	HDProfilePicURLInfo     ImageCandidate   `json:"hd_profile_pic_url_info"`
+	Email                   string           `json:"email"`
+	ExternalURL             string           `json:"external_url"`
 }
 
 // NewAccount creates new account structure
