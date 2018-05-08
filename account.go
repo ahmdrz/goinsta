357b047f054ba1da5a1182cacf62b631c08ef5b6
themester <dgrripoll@gmail.com>
2018-05-08 15:03:38 +0000

    changed account fields

--- account.go
+++ account.go
@@ -5,27 +5,18 @@ type Account struct {
 	inst *Instagram
 
 	// Activity is recent activity
-	Activity *Activity
+	//Activity *Activity
 	// Tray is your disponible friend's stories
-	Tray *Tray
+	//Tray *Tray
 
+	// Account is also a User
 	User
-	//Birthday -> what the hell is ?
-	PhoneNumber             string           `json:"phone_number"`
-	HDProfilePicVersions    []ImageCandidate `json:"hd_profile_pic_versions"`
-	Gender                  int              `json:"gender"`
-	ShowConversionEditEntry bool             `json:"show_conversion_edit_entry"`
-	ExternalLynxURL         string           `json:"external_lynx_url"`
-	Biography               string           `json:"biography"`
-	HDProfilePicURLInfo     ImageCandidate   `json:"hd_profile_pic_url_info"`
-	Email                   string           `json:"email"`
-	ExternalURL             string           `json:"external_url"`
 }
 
 // NewAccount creates new account structure
-func NewAccount(inst *Instagram) (*Account, error) {
+func NewAccount(inst *Instagram) *Account {
 	account := &Account{
 		inst: inst,
 	}
-	return account, nil
+	return account
 }
