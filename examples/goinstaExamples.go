ef23a6fe3aa346fef300cfaab73f430ac8bbb040
themester <dgrripoll@gmail.com>
2018-05-13 18:39:43 +0000

    Changed example package

--- examples/goinstaExamples.go
+++ examples/goinstaExamples.go
@@ -3,6 +3,7 @@ package examples
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/ahmdrz/goinsta"
 	"github.com/howeyc/gopass"
@@ -17,33 +18,32 @@ func CheckErr(err error) {
 	}
 }
 
-func InitGoinsta(min int, msg string) (*goinsta.Instagram, error) {
+// InitGoinsta initilises examples
+func InitGoinsta(msg string) (*goinsta.Instagram, error) {
 	var (
-		nargs  = len(os.Args)
-		config string
-		inst   *goinsta.Instagram
+		// min must be args + config file or username
+		min   = len(strings.Split(msg, ">")) + 1
+		nargs = len(os.Args)
+		user  string
+		inst  *goinsta.Instagram
 	)
 	switch {
 	// this parameters changes
 	case nargs < min:
-		fmt.Printf("%s %s [config file]\n", os.Args[0], msg)
+		fmt.Printf("%s <username or config file> %s\n", os.Args[0], msg)
 		os.Exit(0)
 	case nargs == min+1:
-		config = os.Args[3]
+		user = os.Args[1]
 	}
-	var err error
 
-	if config != "" {
-		inst, err = goinsta.Import(config)
-		if err == nil {
-			UsingSession = true
-		} else {
-			fmt.Println(err)
-			fmt.Print("Using password to login")
-			config = ""
+	if _, err := os.Stat(user); err == nil {
+		inst, err = goinsta.Import(user)
+		if err != nil {
+			return nil, err
 		}
-	}
-	if config == "" {
+
+		UsingSession = true
+	} else {
 		fmt.Print("Password: ")
 		pass, err := gopass.GetPasswd()
 		if err != nil {
