1f98a1585c6d70dd59cb8425fe0bcd317cdf9b48
themester <dgrripoll@gmail.com>
2018-05-13 19:28:27 +0000

    Added activity examples

---
+++ examples/activity/recent.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	e "github.com/ahmdrz/goinsta/examples"
+)
+
+func main() {
+	inst, err := e.InitGoinsta("")
+	e.CheckErr(err)
+
+	act := inst.Activity.Recent()
+	e.CheckErr(err)
+
+	for act.Next() {
+		fmt.Printf("Stories: %d %d\n", len(act.Stories), act.NextID)
+	}
+
+	if !e.UsingSession {
+		err = inst.Logout()
+		e.CheckErr(err)
+	}
+}
