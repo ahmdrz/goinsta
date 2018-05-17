2a0083001f26feb48c048a31fba058bdf0dec644
themester <dgrripoll@gmail.com>
2018-05-17 13:47:24 +0000

    Added Sync function to users and new fields

--- users.go
+++ users.go
@@ -93,56 +93,99 @@ type userResp struct {
 type User struct {
 	inst *Instagram
 
-	ID                         int64        `json:"pk"`
-	Username                   string       `json:"username"`
-	FullName                   string       `json:"full_name"`
-	Biography                  string       `json:"biography"`
-	ProfilePicURL              string       `json:"profile_pic_url"`
-	Email                      string       `json:"email"`
-	PhoneNumber                string       `json:"phone_number"`
-	IsBusiness                 bool         `json:"is_business"`
-	Gender                     int          `json:"gender"`
-	ProfilePicID               string       `json:"profile_pic_id"`
-	HasAnonymousProfilePicture bool         `json:"has_anonymous_profile_picture"`
-	IsPrivate                  bool         `json:"is_private"`
-	IsUnpublished              bool         `json:"is_unpublished"`
-	AllowedCommenterType       string       `json:"allowed_commenter_type"`
-	IsVerified                 bool         `json:"is_verified"`
-	MediaCount                 int          `json:"media_count"`
-	FollowerCount              int          `json:"follower_count"`
-	FollowingCount             int          `json:"following_count"`
-	GeoMediaCount              int          `json:"geo_media_count"`
-	ExternalURL                string       `json:"external_url"`
-	HasBiographyTranslation    bool         `json:"has_biography_translation"`
-	ExternalLynxURL            string       `json:"external_lynx_url"`
-	HdProfilePicURLInfo        PicURLInfo   `json:"hd_profile_pic_url_info"`
-	HdProfilePicVersions       []PicURLInfo `json:"hd_profile_pic_versions"`
-	UsertagsCount              int          `json:"usertags_count"`
-	HasChaining                bool         `json:"has_chaining"`
-	IsFavorite                 bool         `json:"is_favorite"`
-	ReelAutoArchive            string       `json:"reel_auto_archive"`
-	School                     School       `json:"school"`
-	PublicEmail                string       `json:"public_email"`
-	PublicPhoneNumber          string       `json:"public_phone_number"`
-	PublicPhoneCountryCode     string       `json:"public_phone_country_code"`
-	ContactPhoneNumber         string       `json:"contact_phone_number"`
-	Byline                     string       `json:"byline"`
-	SocialContext              string       `json:"social_context,omitempty"`
-	SearchSocialContext        string       `json:"search_social_context,omitempty"`
-	MutualFollowersCount       float64      `json:"mutual_followers_count"`
-	LatestReelMedia            int          `json:"latest_reel_media,omitempty"`
-	CityID                     int64        `json:"city_id"`
-	CityName                   string       `json:"city_name"`
-	AddressStreet              string       `json:"address_street"`
-	DirectMessaging            string       `json:"direct_messaging"`
-	Latitude                   float64      `json:"latitude"`
-	Longitude                  float64      `json:"longitude"`
-	Category                   string       `json:"category"`
-	BusinessContactMethod      string       `json:"business_contact_method"`
-	IsCallToActionEnabled      bool         `json:"is_call_to_action_enabled"`
-	FbPageCallToActionID       string       `json:"fb_page_call_to_action_id"`
-	Zip                        string       `json:"zip"`
-	Friendship                 Friendship   `json:"friendship_status"`
+	ID                         int64  `json:"pk"`
+	Username                   string `json:"username"`
+	FullName                   string `json:"full_name"`
+	Biography                  string `json:"biography"`
+	ProfilePicURL              string `json:"profile_pic_url"`
+	Email                      string `json:"email"`
+	PhoneNumber                string `json:"phone_number"`
+	IsBusiness                 bool   `json:"is_business"`
+	Gender                     int    `json:"gender"`
+	ProfilePicID               string `json:"profile_pic_id"`
+	HasAnonymousProfilePicture bool   `json:"has_anonymous_profile_picture"`
+	IsPrivate                  bool   `json:"is_private"`
+	IsUnpublished              bool   `json:"is_unpublished"`
+	AllowedCommenterType       string `json:"allowed_commenter_type"`
+	IsVerified                 bool   `json:"is_verified"`
+	MediaCount                 int    `json:"media_count"`
+	FollowerCount              int    `json:"follower_count"`
+	FollowingCount             int    `json:"following_count"`
+	FollowingTagCount          int    `json:"following_tag_count"`
+	GeoMediaCount              int    `json:"geo_media_count"`
+	ExternalURL                string `json:"external_url"`
+	HasBiographyTranslation    bool   `json:"has_biography_translation"`
+	ExternalLynxURL            string `json:"external_lynx_url"`
+	BiographyWithEntities      struct {
+		RawText  string        `json:"raw_text"`
+		Entities []interface{} `json:"entities"`
+	} `json:"biography_with_entities"`
+	UsertagsCount                int          `json:"usertags_count"`
+	HasChaining                  bool         `json:"has_chaining"`
+	IsFavorite                   bool         `json:"is_favorite"`
+	IsFavoriteForStories         bool         `json:"is_favorite_for_stories"`
+	IsFavoriteForHighlights      bool         `json:"is_favorite_for_highlights"`
+	CanBeReportedAsFraud         bool         `json:"can_be_reported_as_fraud"`
+	ShowShoppableFeed            bool         `json:"show_shoppable_feed"`
+	ShoppablePostsCount          int          `json:"shoppable_posts_count"`
+	ReelAutoArchive              string       `json:"reel_auto_archive"`
+	HasHighlightReels            bool         `json:"has_highlight_reels"`
+	PublicEmail                  string       `json:"public_email"`
+	PublicPhoneNumber            string       `json:"public_phone_number"`
+	PublicPhoneCountryCode       string       `json:"public_phone_country_code"`
+	ContactPhoneNumber           string       `json:"contact_phone_number"`
+	CityID                       int64        `json:"city_id"`
+	CityName                     string       `json:"city_name"`
+	AddressStreet                string       `json:"address_street"`
+	DirectMessaging              string       `json:"direct_messaging"`
+	Latitude                     float64      `json:"latitude"`
+	Longitude                    float64      `json:"longitude"`
+	Category                     string       `json:"category"`
+	BusinessContactMethod        string       `json:"business_contact_method"`
+	IncludeDirectBlacklistStatus bool         `json:"include_direct_blacklist_status"`
+	HdProfilePicURLInfo          PicURLInfo   `json:"hd_profile_pic_url_info"`
+	HdProfilePicVersions         []PicURLInfo `json:"hd_profile_pic_versions"`
+	UsertagsCount                int          `json:"usertags_count"`
+	HasChaining                  bool         `json:"has_chaining"`
+	IsFavorite                   bool         `json:"is_favorite"`
+	ReelAutoArchive              string       `json:"reel_auto_archive"`
+	School                       School       `json:"school"`
+	PublicEmail                  string       `json:"public_email"`
+	PublicPhoneNumber            string       `json:"public_phone_number"`
+	PublicPhoneCountryCode       string       `json:"public_phone_country_code"`
+	ContactPhoneNumber           string       `json:"contact_phone_number"`
+	Byline                       string       `json:"byline"`
+	SocialContext                string       `json:"social_context,omitempty"`
+	SearchSocialContext          string       `json:"search_social_context,omitempty"`
+	MutualFollowersCount         float64      `json:"mutual_followers_count"`
+	LatestReelMedia              int          `json:"latest_reel_media,omitempty"`
+	CityID                       int64        `json:"city_id"`
+	CityName                     string       `json:"city_name"`
+	AddressStreet                string       `json:"address_street"`
+	DirectMessaging              string       `json:"direct_messaging"`
+	Latitude                     float64      `json:"latitude"`
+	Longitude                    float64      `json:"longitude"`
+	Category                     string       `json:"category"`
+	BusinessContactMethod        string       `json:"business_contact_method"`
+	IsCallToActionEnabled        bool         `json:"is_call_to_action_enabled"`
+	FbPageCallToActionID         string       `json:"fb_page_call_to_action_id"`
+	Zip                          string       `json:"zip"`
+	Friendship                   Friendship   `json:"friendship_status"`
+}
+
+// Sync updates user info
+func (user *User) Sync() error {
+	insta := user.inst
+	body, err := insta.sendSimpleRequest(urlUserInfo, user.ID)
+	if err == nil {
+		resp := userResp{}
+		err = json.Unmarshal(body, &resp)
+		if err == nil {
+			*user = resp.User
+			user.inst = inst
+		}
+	}
+	return err
 }
 
 // Following returns a list of user following.
