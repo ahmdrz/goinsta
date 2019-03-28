c2e197ee3280e24ff30b7c11d5e87af6a5553314
Altan Özlü <we.are.dev.tr@gmail.com>
2019-03-28 15:42:29 +0300

    Update const.go

--- const.go
+++ const.go
@@ -58,7 +58,7 @@ const (
 	urlUserUnfollow      = "friendships/destroy/%d/"
 	urlUserFeed          = "feed/user/%d/"
 	urlFriendship        = "friendships/show/%d/"
-	urlFriendshipPending = "friendships/pending?"
+	urlFriendshipPending = "friendships/pending/"
 	urlUserStories       = "feed/user/%d/reel_media/"
 	urlUserTags          = "usertags/%d/feed/"
 	urlBlockedList       = "users/blocked_list/"
