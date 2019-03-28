a20f833cf5fdfb351265a2dceb3828f76c925622
Altan Özlü <we.are.dev.tr@gmail.com>
2019-03-28 15:38:33 +0300

    Update const.go

--- const.go
+++ const.go
@@ -44,7 +44,6 @@ const (
 	urlFeedSaved        = "feed/saved/"
 	urlSetBiography     = "accounts/set_biography/"
 	urlFeedLiked        = "feed/liked/"
-	urlPendingFollowers = "friendships/pending?"
 
 	// account and profile
 	urlFollowers = "friendships/%d/followers/"
@@ -59,7 +58,7 @@ const (
 	urlUserUnfollow      = "friendships/destroy/%d/"
 	urlUserFeed          = "feed/user/%d/"
 	urlFriendship        = "friendships/show/%d/"
-	urlFriendshipPending = "friendships/pending/"
+	urlFriendshipPending = "friendships/pending?"
 	urlUserStories       = "feed/user/%d/reel_media/"
 	urlUserTags          = "usertags/%d/feed/"
 	urlBlockedList       = "users/blocked_list/"
