f88b7fcbaa0b775e6f07eec4f81a4ba73d4a47ef
Ahmadreza Zibaei <zibaeiahmadreza@gmail.com>
2019-07-29 09:52:23 +0430

    count in user-search query

---
+++ tests/search_test.go
@@ -0,0 +1,32 @@
+package tests
+
+import (
+	"testing"
+)
+
+func TestSearchUser(t *testing.T) {
+	count := 20
+
+	insta, err := getRandomAccount()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	result, err := insta.Search.User("a", count)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	if result.Status != "ok" {
+		t.Fatal(result.Status)
+		return
+	}
+	t.Logf("result length is %d", len(result.Users))
+	if len(result.Users) < count {
+		t.Fatalf("length is less than %d", count)
+		return
+	}
+	for _, user := range result.Users {
+		t.Logf("user %s with id %d\n", user.Username, user.ID)
+	}
+}
